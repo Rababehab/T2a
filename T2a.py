3f71b3f3315151e5b86f54d52147075ab91f88c0
Rababehab <777rababehab@gmail.com>
2025-09-10 12:09:32 +0300

    Add files via upload

---
+++ T2a.py
@@ -0,0 +1,7 @@
+Contacts= {"Rabab": "01154454545" ,"Mohamed":"0123554558" , "Ehab":"0126556666"}
+new_contacts= {"Samar":"01245766656","Ahmed":"012465886" ,"Asser": "01245658565"}
+Contacts.update(new_contacts)
+print(Contacts)
+print(Contacts.keys())
+print(Contacts.values())
+print(Contacts.items())
